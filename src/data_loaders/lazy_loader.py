15820ff741b6b6c9a67a326e867b7630d4b8ccfd
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-04-13 21:04:04 -0400

    add docstrings to dataloader

--- src/data_loaders/lazy_loader.py
+++ src/data_loaders/lazy_loader.py
@@ -16,13 +16,14 @@ class LazyLoader(BaseDataLoader):
         self._map_dir()
 
     def _map_dir(self) -> None:
+        """creates the `_path_data` attribute to store directory structure
+        """
         dates = os.listdir(self._root_dir)
         dates = [dir_ for dir_ in dates if os.path.isdir(self._root_dir / dir_)]
 
         path_data = {}
         for date_ in dates:
             path_data[date_] = {}
-            strike_fns = []
             for fn in os.listdir(self._root_dir / date_):
                 strike = fn.split(".")[0]
                 if ".csv" not in fn:
@@ -34,13 +35,29 @@ class LazyLoader(BaseDataLoader):
 
         self._path_data = path_data
 
+    # TODO: add error handling in case file no longer exists
     def query(self, date: str, strike: Union[int, str]) -> pd.DataFrame:
+        """Gets data for the specified date and strike
+
+        Args:
+            date (str): Event date
+            strike (Union[int, str]): contract strike
+
+        Returns:
+            pd.DataFrame: _description_
+        """
         path = self._path_data[date][str(strike)]
         data = pd.read_csv(path)
 
         return data
     
     def sample(self) -> pd.DataFrame:
+        """Uniformly samples a date, then uniformly samples a strike. Note: this
+          is not a uniform distribution across all valid (date, strike) pairs
+
+        Returns:
+            pd.DataFrame: data for random date and strike
+        """
         random_date = random_choice(list(self._path_data.keys()))
         random_strike = random_choice(list(self._path_data[random_date].keys()))
 
