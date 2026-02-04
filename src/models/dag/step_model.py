a47b67a891aff2da46dc920c1ca3a90f94b7fe84
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2026-02-04 12:02:04 -0500

    implement greeks for dag model

--- src/models/dag/step_model.py
+++ src/models/dag/step_model.py
@@ -1,15 +1,92 @@
+from typing import Optional, Dict
+
 
 from src.base import BaseModel
-from . _utils import DAG
+from . _utils import DAG, _build_tree
 
+# NOTE: can probably estimate gamma vega theta numerically, but 
+# this will exas errors if tree is not sufficiently deep
 
-# TODO: estimate up/down from mu and sigma implied
-# TODO: fill inv alue and greeks from _utils
 class DAGStepModel(BaseModel):
+    dag: Optional[DAG]
 
-    def __init__(self, strike: int, expiration: float):
-        super().__init__(strike, expiration)
+    def __init__(
+        self, 
+        strike: int, 
+        expiration: float, 
+        u: float,
+        depth=10,
+        rate=0,
+    ):
+        self.strike = strike
+        self.expiration = expiration
+        self.u = u
+        self.depth = depth
+        self.rate=rate
 
-        if not isinstance(strike, int):
-            raise TypeError("`strike` must be an int for step contracts")
+        self.dag = None
+
+    @staticmethod
+    def _value(
+        u_price: float, 
+        strike: float,
+        u,
+        depth: int
+    ) -> float:
+        tree = _build_tree(u_price, strike, u, depth)
+        return  tree.calc_node_deriv_value(rate=0)
+    
+    @staticmethod
+    def _iv(*_, **__) -> float:
+        raise NotImplementedError("_iv not implemnted for DAG model")
+    
+    @staticmethod
+    def _delta(
+        u_price: float, 
+        strike: float,
+        u,
+        depth: int
+    ) -> float:
+        tree = _build_tree(u_price, strike, u, depth)
+        return  tree.calc_node_deriv_value(rate=0)
+    
+    @staticmethod
+    def _vega(*_, **__) -> float:
+        raise NotImplementedError("_vega not implemnted for DAG model")
+    
+    @staticmethod
+    def _theta(*_, **__) -> float:
+        raise NotImplementedError("_theta not implemnted for DAG model")
+    
+    @staticmethod
+    def _gamma(*_, **__) -> float:
+        raise NotImplementedError("_gamma not implemnted for DAG model")
+
+    @classmethod
+    def __call__(
+        cls,
+        u_price: float, 
+        strike: float,
+        u,
+        depth: int
+    ) -> Dict[str, float]:
+        tree = _build_tree(u_price, strike, u, depth)
+        iv = None
+        value = tree.calc_node_deriv_value(rate=0)
+        delta = tree.delta
+        vega = None
+        theta = None
+        gamma = None
+
+        return {
+            "value": value,
+            "iv": iv,
+            "delta": delta,
+            "vega": vega,
+            "theta": theta,
+            "gamma": gamma
+        }
+
+
+
         
