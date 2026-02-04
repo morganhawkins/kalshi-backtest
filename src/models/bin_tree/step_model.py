13109fe8378717cda4d950a6882a4ffe38b7751e
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2026-02-04 12:41:11 -0500

    add note abt to bin tree model

--- src/models/bin_tree/step_model.py
+++ src/models/bin_tree/step_model.py
@@ -2,7 +2,8 @@
 from src.base import BaseModel
 from . _utils import BinaryTree
 
-
+# NOTE: noit sure hwo dag would work with path dependent terminal 
+# values, so this would be a solution to this. painfully slow though
 # TODO: estimate up/down factors from mu and sigma implied
 # TODO: fill in value and greek exposure from _utils
 class BTStepModel(BaseModel):
